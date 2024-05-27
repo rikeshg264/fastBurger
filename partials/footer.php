0632a6e37c43b466f1f8864b1f28d221c25d686b
rated264 <rikesh.gurung.10i.esac@gmail.com>
2024-05-28 00:06:41 +0100

    register and login

--- partials/footer.php
+++ partials/footer.php
@@ -3,8 +3,8 @@
 <div class=" bg-gray-900">
     <div class="max-w-2xl mx-auto text-white py-10">
         <div class="text-center">
-            <h3 class="text-3xl mb-3"> Download our fitness app </h3>
-            <p> Stay fit. All day, every day. </p>
+            <h3 class="text-3xl mb-3"> Download our fastBurger app </h3>
+            <p> Feels like heaven, Every bite. </p>
             <div class="flex justify-center my-10">
                 <div class="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
                     <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8">
@@ -23,7 +23,7 @@
             </div>
         </div>
         <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-            <p class="order-2 md:order-1 mt-8 md:mt-0"> &copy; Beautiful Footer, 2021. </p>
+            <p class="order-2 md:order-1 mt-8 md:mt-0"> &copy; Fastburgers 2024 </p>
             <div class="order-1 md:order-2">
                 <span class="px-2">About us</span>
                 <span class="px-2 border-l">Contact us</span>
@@ -32,6 +32,5 @@
         </div>
     </div>
 </div>
-<p> &copy; Fastburgers 2024<p> 
 </body>
 </html>
